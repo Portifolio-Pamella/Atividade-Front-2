ffcf25c3558498829b31dd16ddd1461418c8feda
pamellachristiny <pamella.christiny2006@gmail.com>
2025-09-30 13:31:39 -0300

    config: adiciona e configura tailwind.config.js e postcss.config.js

--- access-control-cp/tailwind.config.js
+++ access-control-cp/tailwind.config.js
@@ -1,8 +1,9 @@
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}", // Essencial para o Tailwind ver o código
   ],
   theme: {
     extend: {},
